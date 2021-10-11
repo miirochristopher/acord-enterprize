cb27f68e10661239f356207c9877ae273325be47
Miiro Christopher <chrismiiroslav@gmail.com>
2021-10-11 11:57:17 +0300

    Access to locked resources

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ cd acord-uganda/
 
 mvn clean install
 
+mvn clean dependency:copy-dependencies package
+
+java -jar target/acord-uganda-1.0.0.jar 
+
 mvn spring-boot:run
--- src/main/java/com/acord/config/SecurityConfiguration.java
+++ src/main/java/com/acord/config/SecurityConfiguration.java
@@ -13,33 +13,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/","/resources/**", "/h2/**","/webjars/**");
+		web.ignoring().antMatchers("/**", "/resources/**", "/h2/**", "/webjars/**");
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("admin").password("{noop}admin").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf()
-			.disable()
-			.authorizeRequests()
-			.antMatchers("/beneficiary/**")
-				.hasAnyRole("ADMIN")
-			.and()
-			.formLogin()
-				.loginPage("/login")
-				.loginProcessingUrl("/login")
-				.defaultSuccessUrl("/beneficiary/form", true)
-				.failureUrl("/login?error=true")
-				.permitAll()
-			.and()
-		       .logout()
-		            .logoutUrl("/logout")
-					.logoutSuccessUrl("/login")
-			        .permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers("/beneficiary/**").hasAnyRole("ADMIN").and().formLogin()
+				.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/beneficiary/form", true)
+				.failureUrl("/login?error=true").permitAll().and().logout().logoutUrl("/logout")
+				.logoutSuccessUrl("/login").permitAll();
 	}
 }
